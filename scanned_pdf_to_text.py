12f0160ddc2547f0f109a6f92c5000c66ad40a8d
ravi944 <108458982+ravi944@users.noreply.github.com>
2022-07-20 14:47:37 +0530

    Update scanned_pdf_to_text.py

--- scanned_pdf_to_text.py
+++ scanned_pdf_to_text.py
@@ -1,8 +1,10 @@
+import pytesseract
 from pdf2image import convert_from_path
-from pytesseract import image_to_string
-from PIL import Image
-converted_scan = convert_from_path('bank-statement-template-11-converted.pdf', 500)
-for i in converted_scan:
-    i.save('scan_image.png', 'png')
-text = image_to_string(Image.open('scan_image.png'))
-print(text)
+import glob
+
+pdfs = glob.glob(r"bharat_co_bank-pdfdoctor_com.pdf")
+for pdf_path in pdfs:
+    pages = convert_from_path(pdf_path, 500)
+    for pageNum,imgBlob in enumerate(pages):
+        text = pytesseract.image_to_string(imgBlob,lang='eng')
+        print(text)
